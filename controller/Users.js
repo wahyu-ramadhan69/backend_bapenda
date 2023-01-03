96865ccf3c7bf76e776cc314a54c22a88520381c
wahyu ramadhan <wahyu.ramadhani6969@gmail.com>
2023-01-03 20:33:37 +0700

    second commit

--- controller/Users.js
+++ controller/Users.js
@@ -127,6 +127,7 @@ export const Login = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
+      secure: true,
     });
     res.json({ accessToken });
   } catch (error) {
